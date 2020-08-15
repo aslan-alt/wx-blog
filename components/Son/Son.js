86b19c6e1954468c6e83ac88f57cffa0b733b666
aslan-alt <874470891@qq.com>
2020-08-16 03:28:39 +0800

    使用youzan前备份

---
+++ components/Son/Son.js
@@ -0,0 +1,28 @@
+// components/Son/Son.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    money:{
+      type:Number
+    },
+   
+  },
+  methods:{
+    xxx(){
+      console.log(this)
+    }
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+ 
+})
